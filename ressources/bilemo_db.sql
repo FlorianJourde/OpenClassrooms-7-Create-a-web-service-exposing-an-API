c14ce5836e52409361dfcce8924b7322cf64812d
Florian Jourde <jourdeflorian@gmail.com>
2023-02-03 15:34:53 +0100

    Add fixtures and database to project

---
+++ ressources/bilemo_db.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : ven. 03 fév. 2023 à 14:32
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bilemo_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_C7440455E7927C74` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`id`, `email`, `roles`, `password`) VALUES
+(1, 'client0@bilemo.com', '[\"ROLE_USER\"]', '$2y$13$gzowZU3GCMrc8IAuyR11I.Tyk5/L2Xj5rU1OS8H9cb1VYQnR0DuPy'),
+(2, 'client1@bilemo.com', '[\"ROLE_USER\"]', '$2y$13$DkCAza5uiKDZXcmUatrrTe8tRZyfiIcQM5fXtfrVVlAE/13WtxmPG'),
+(3, 'client2@bilemo.com', '[\"ROLE_USER\"]', '$2y$13$jeOW5rDdqWUlPedEFi0hiuQ6IhS31UwjZmJGiWEs3ktGJuWlyqus2');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20230130215046', '2023-01-30 21:51:01', 54);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `phone`
+--
+
+DROP TABLE IF EXISTS `phone`;
+CREATE TABLE IF NOT EXISTS `phone` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `brand` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `model` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `color` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `memory` int(11) DEFAULT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci,
+  `price` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `phone`
+--
+
+INSERT INTO `phone` (`id`, `brand`, `model`, `color`, `memory`, `description`, `price`) VALUES
+(11, 'Google', '14', 'Red', 128, 'This is the phone you need', 999.99),
+(12, 'Apple', '13 Ultra', 'Black', 64, 'The best smartphone overall', 249.99),
+(13, 'Samsung', 'S22', 'Green', 32, 'Once you\'ve tried it, you can\'t go back', 149.99),
+(14, 'Xiaomi', 'X6 Pro', 'Black', 32, 'Our brand new flagship', 999.99),
+(15, 'Samsung', 'X6 Pro', 'Grey', 128, 'Simply different', 149.99),
+(16, 'Xiaomi', '14', 'White', 512, 'Once you\'ve tried it, you can\'t go back', 499.99),
+(17, 'Xiaomi', '13 Ultra', 'Black', 64, 'This is the phone you need', 999.99),
+(18, 'Google', '14', 'Black', 512, 'Our brand new flagship', 499.99),
+(19, 'Google', 'S22', 'Red', 32, 'The best smartphone overall', 149.99),
+(20, 'Google', '13 Ultra', 'Red', 32, 'Simply different', 149.99);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `creation_date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_8D93D64919EB6921` (`client_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `client_id`, `email`, `creation_date`) VALUES
+(1, 3, 'user0@bilemo.com', '2023-02-03 14:31:24'),
+(2, 2, 'user1@bilemo.com', '2023-02-03 14:31:24'),
+(3, 2, 'user2@bilemo.com', '2023-02-03 14:31:24'),
+(4, 1, 'user3@bilemo.com', '2023-02-03 14:31:24'),
+(5, 3, 'user4@bilemo.com', '2023-02-03 14:31:24'),
+(6, 2, 'user5@bilemo.com', '2023-02-03 14:31:24'),
+(7, 1, 'user6@bilemo.com', '2023-02-03 14:31:24'),
+(8, 2, 'user7@bilemo.com', '2023-02-03 14:31:24'),
+(9, 3, 'user8@bilemo.com', '2023-02-03 14:31:24'),
+(10, 2, 'user9@bilemo.com', '2023-02-03 14:31:24');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `FK_8D93D64919EB6921` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,63 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Client;
+use App\Entity\Phone;
+use App\Entity\User;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+//use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Config\Security\PasswordHasherConfig;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHaser;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHaser = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $clientList = [];
+
+        for ($i = 0; $i < 3; $i++) {
+            $client = new Client();
+            $client->setEmail('client' . $i . '@bilemo.com');
+            $client->setRoles(['ROLE_USER']);
+            $client->setPassword($this->userPasswordHaser->hashPassword($client, '123456'));
+            $manager->persist($client);
+            $clientList[] = $client;
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail('user' . $i . '@bilemo.com');
+            $user->setCreationDate(new DateTimeImmutable());
+            $user->setClient($clientList[array_rand($clientList)]);
+            $manager->persist($user);
+        }
+
+        $brands = ['Apple', 'Samsung', 'Google', 'Xiaomi', 'Oppo'];
+        $models = ['14', '7 Plus', 'S22', '13 Ultra', 'X6 Pro'];
+        $colors = ['Black', 'White', 'Grey', 'Red', 'Blue', 'Yellow', 'Green'];
+        $memory = [32, 64, 128, 256, 512];
+        $descriptions = ['Our brand new flagship', 'This is the phone you need', 'Once you\'ve tried it, you can\'t go back', 'The best smartphone overall', 'Simply different'];
+        $prices = [249.99, 499.99, 999.99, 1299.99, 149.99];
+
+        for ($i = 0; $i < 10; $i++) {
+            $phone = new Phone();
+            $phone->setBrand($brands[array_rand($brands, 1)]);
+            $phone->setModel($models[array_rand($models, 1)]);
+            $phone->setColor($colors[array_rand($colors, 1)]);
+            $phone->setMemory($memory[array_rand($memory, 1)]);
+            $phone->setDescription($descriptions[array_rand($descriptions, 1)]);
+            $phone->setPrice($prices[array_rand($prices, 1)]);
+            $manager->persist($phone);
+        }
 
         $manager->flush();
     }
